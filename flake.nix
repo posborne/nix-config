dc3217011ced7be22c7dd79c188ca2b98de0e6e8
Paul Osborne <osbpau@gmail.com>
2024-11-23 18:16:47 -0600

    Add home-manager

--- flake.nix
+++ flake.nix
@@ -4,17 +4,27 @@
   inputs = {
     # principle inputs
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    # home-manager.url = "github:nix-community/home-manager";
-    # home-manager.inputs.nixpkgs.follows = "nixpkgs";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
     # flake-parts.url = "github:hercules-ci/flake-parts";
     # nixos-unified.url = "github:srid/nixos-unified";
   };
 
-  outputs = { self, nixpkgs, ... }@inputs: {
+  outputs = { self, nixpkgs, home-manager, ... }@inputs: {
     nixosConfigurations.default = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
+      specialArgs = { inherit inputs; }; # set inputs as submodule special args
       modules = [
         ./configuration.nix
+	
+	home-manager.nixosModules.home-manager
+	{
+	  home-manager.useGlobalPkgs = true;
+	  home-manager.useUserPackages = true;
+	  home-manager.users.posborne = import ./home.nix;
+        }
       ];
     };
   };
---
+++ home.nix
@@ -0,0 +1,18 @@
+{ config, pkgs, ... }:
+
+{
+  home.username = "posborne";
+  home.homeDirectory = "/home/posborne";
+
+  programs.git = {
+    enable = true;
+    userName = "Paul Osborne";
+    userEmail = "osbpau@gmail.com";
+  };
+
+  home.packages = with pkgs; [
+    neofetch
+  ];
+
+  home.stateVersion = "24.11";
+}
