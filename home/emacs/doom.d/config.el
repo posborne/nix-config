8f07b62efc9985db6787b15680ae7e7324f85dd9
Paul Osborne <osbpau@gmail.com>
2024-12-03 18:02:32 -0600

    emacs: add keybind to hide/show inline type hints with eglot

--- home/emacs/doom.d/config.el
+++ home/emacs/doom.d/config.el
@@ -1,5 +1,15 @@
 ;;; config.el --- Description -*- lexical-binding: t; -*-
 ;;; $DOOMDIR/config.el
 
+
+;; Setup doom themes and fonts
 (setq doom-theme 'doom-palenight)
 (setq doom-font (font-spec :family "CaskaydiaCove Nerd Font" :size 14 :weight 'medium))
+
+
+;; Additional keybinds
+(map! "C-c c TAB" #'eglot-inlay-hints-mode)
+
+
+;; Close the compile window automatically if it finishes
+;; successfully; otherwise it just gets in the way.
